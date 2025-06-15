09a0db3682b169d8b226f337c16d7ba27aa8ac29
ajay-jangid-github <ajaykumarjangid957@gmail.com>
2025-06-15 17:58:47 +0530

    frontend and backend file

--- frontend-backup/src/pages/About.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-const About = () => {
-  return (
-    <div className="p-8 text-center text-2xl font-bold">
-      Welcome to ITCompany.com Home Page
-    </div>
-  );
-};
-export default About;
